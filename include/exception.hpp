dd7e1b4d9402c242a9535233e4988a4dd075f2bb
Colby <conish17913@gmail.com>
2023-08-02 01:16:26 -0400

    added custom exception class

---
+++ include/exception.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+#include <exception>
+
+namespace ofl
+{
+    class exception : public std::exception
+    {
+    public:
+        exception(std::string message) :_message(message) { }
+        virtual const char* what() const throw()
+        {
+            return _message.c_str();
+        }
+    private:
+        std::string _message;
+
+        exception();
+    };
+
+    class parser_exception : public exception 
+    {
+    public:
+        parser_exception(std::string message) :exception(message) {}
+    };
+}
